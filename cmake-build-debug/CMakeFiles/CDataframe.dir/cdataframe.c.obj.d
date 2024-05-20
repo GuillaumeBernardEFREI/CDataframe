6a4169d4732236cea6708599cea55b10a4e99e8f
Samu7l <samuel.molliere@efrei.net>
2024-05-20 12:12:38 +0200

    J'ai fait la fonction create_cdataframe dit moi ce que tu en penses, j'ai aussi mit les fonctions qui sont à la fin du doc dans list.c et et list.h

---
+++ cmake-build-debug/CMakeFiles/CDataframe.dir/cdataframe.c.obj.d
@@ -0,0 +1,5 @@
+CMakeFiles/CDataframe.dir/cdataframe.c.obj: \
+ C:\Users\samue\CLionProjects\CDataframe-Guigui\cdataframe.c \
+ C:\Users\samue\CLionProjects\CDataframe-Guigui\cdataframe.h \
+ C:\Users\samue\CLionProjects\CDataframe-Guigui\column.h \
+ C:\Users\samue\CLionProjects\CDataframe-Guigui\list.h
---
+++ cmake-build-debug/CMakeFiles/CDataframe.dir/list.c.obj.d
@@ -0,0 +1,3 @@
+CMakeFiles/CDataframe.dir/list.c.obj: \
+ C:\Users\samue\CLionProjects\CDataframe-Guigui\list.c \
+ C:\Users\samue\CLionProjects\CDataframe-Guigui\list.h
